a3703c61de9c93d35b9df64834cb4dab983154f8
Marwan Diab <90741989+Marwan-xDiab@users.noreply.github.com>
2022-02-14 12:47:03 +0200

    Add files via upload

---
+++ RentBuy/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data.Sql;
+namespace RentBuy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string conictionstring = "";
+        public MainWindow()
+        {
+            InitializeComponent();
+            conictionstring = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\RentBuy.mdf;Integrated Security=True";
+        }
+
+
+
+      
+        
+        private void save_Click(object sender, RoutedEventArgs e)
+        {
+            AddItem("insert into Rent_buy VALUES (N'" + Name.Text + "',N'" + Phone.Text + "',N'" + Addres.Text + "',N'" + PersonalID.Text + "',N'" + Resualt.Text + "',N'" + DateTime.Now.ToString() + "',N'" + Note.Text + "');");
+        }
+
+        public void AddItem(string qury)
+        {
+           
+            try
+            {
+                using (SqlConnection con = new SqlConnection(conictionstring))
+                {
+                    con.Open();
+                    using (SqlCommand command = new SqlCommand(qury, con))
+                    {
+                        command.ExecuteNonQuery();
+                        MessageBox.Show("Succseful Added");
+                        this.Close();
+                    }
+                    con.Close();
+                }
+            }
+            catch (SystemException ex)
+            {
+                MessageBox.Show(string.Format("An error occurred: {0}", ex.Message));
+            }
+
+        }
+
+        private void getruselt_Click(object sender, RoutedEventArgs e)
+        {
+            if (Q1.IsChecked.Value == true && Q2.IsChecked.Value == true && Q3.IsChecked.Value == true &&
+                Q4.IsChecked.Value == true && Q5.IsChecked.Value == false && Q6.IsChecked.Value == true
+                && Q7.IsChecked.Value == true && Q8.IsChecked.Value == true && Q9.IsChecked.Value == true)
+            {
+                Resualt.Text = "Buy";
+            }
+            else
+
+            { Resualt.Text = "Rent"; }
+            save.IsEnabled = true;
+            
+        }
+    }
+
+}
+    
---
+++ SQLQuery1.sql
@@ -0,0 +1,12 @@
+﻿create table Rent_buy 
+(
+	CoustmorID int primary key identity(1,1),
+	FullName varchar(50),
+	phone varchar(15),
+	Addres varchar(120),
+	personalID varchar(16),
+	Resualt varchar(10),
+	Date_time varchar(30),
+	Note varchar(200)
+
+);
